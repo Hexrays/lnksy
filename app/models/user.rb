31b27bfd9d743f224ccf9c1a579f2f5367c2444c
hexrays <jeffdphoto@gmail.com>
2013-05-15 09:09:53 -0400

    Corrected user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :name, :password, :username,  :on => :create
   validates_confirmation_of :password
   validates :password, :length => { :in => 6..20 }
-  validates :username, :uniqueness => { :case_sensitive => false }
-  validates :email, :uniqueness => { case_sensitive: false }
+  validates_uniqueness_of :username, :case_sensitive => false
+  validates_uniqueness_of :email, :case_sensitive => false
   validates :name, :username, :length => { :minimum => 2 }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   has_many :posts
