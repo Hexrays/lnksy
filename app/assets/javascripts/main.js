b822ed2ea45e1a2edd8b174335906d2f96f0ff64
hexrays <jeffdphoto@gmail.com>
2013-05-17 01:42:16 -0400

    Added styling updates

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
 	linkList.slideDown(750);
 
 	for (var i = 0; i<liCount; i++){
-		// console.log('the opacity is now '+ opacity);
 		liColor = 'rgba(0,188,235, '+opacity+')';
 		$(".link-list li:nth-child("+i+")").css('background-color', liColor);
 		opacity -= colorSteps;
@@ -23,6 +22,24 @@ $(document).ready(function(){
 	$('h3.new-folder').on('click', function(){
 		folderField.slideToggle();
 	});
+
+	$('li').hover(function(){
+		$(this).find('.hide').toggle();
+	});
+
+	// jQuery.fn.rollDown = function( speed, easing, callback ) {
+		// for (var i = 0; i < liCount; i++) {
+		// 	$("ul.link-list li:nth-child("+i+")").toggle();
+		// };
+
+		// return $this.slideDown(500, function(){
+		// 	$this.delay(2000).slideUp(500);
+		// });	
+	// };
+
+	// linkList.rollDown();
+
+
 
 });
 
