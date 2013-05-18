45eeaec9555c3811b356180aca9c1890045b543b
hexrays <jeffdphoto@gmail.com>
2013-05-17 23:38:33 -0400

    Corrected email case sensitivity on login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation, :username
   attr_accessor :password
   before_save :encrypt_password
+  before_save do self.email.downcase! end
   after_create :default_folder
 
 
@@ -9,7 +10,7 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates :password, :length => { :in => 6..20 }
   validates_uniqueness_of :username, :case_sensitive => false
-  validates_uniqueness_of :email, :case_sensitive => false
+  validates :email, :uniqueness => { :case_sensitive => false }
   validates :name, :username, :length => { :minimum => 2 }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   has_many :posts
@@ -23,8 +24,12 @@ class User < ActiveRecord::Base
     self.folders.create(:name => "LNKS!")
   end
 
+  def self.find_by_lower_email(email)
+    User.find_by_email(email.downcase)
+  end
+
   def self.authenticate(email, password)
-    user = find_by_email(email)
+    user = find_by_lower_email(email)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
