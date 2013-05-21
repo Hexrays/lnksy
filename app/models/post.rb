3f3d7931e9b882d2cfc88e0dfb0c07234ffccdfa
hexrays <jeffdphoto@gmail.com>
2013-05-20 22:47:15 -0400

    Allowed links to be submitted without http://.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
 	default_scope order: 'created_at DESC'
   attr_accessible :description, :folder_id, :link_url, :title, :user_id
+  before_validation :add_http
   before_validation :get_title
   validates :link_url, presence: true
   validates :folder_id, presence: true
@@ -9,10 +10,10 @@ class Post < ActiveRecord::Base
 	require 'nokogiri'
 	require 'open-uri'
 
-def strip_url
-  uri = URI.parse(self.link_url)
-  uri.host
-end
+  def strip_url
+    uri = URI.parse(self.link_url)
+    uri.host
+  end
 
   private
 
@@ -20,4 +21,14 @@ end
   	doc = Nokogiri::HTML(open(self.link_url))
 		self.title = doc.css('title').text
   end
+
+  def add_http
+    if self.link_url =~ /^https?:\/\//
+      self.link_url
+    else
+      self.link_url = "http://" + self.link_url
+    end
+  end
+
 end
+
